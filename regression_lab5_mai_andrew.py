1f324a52bbc84720826c0778c0d1495f9ab2d7c2
Arlios <luyen_mai@Live.com>
2018-10-18 20:33:42 -0700

    Regression edits

--- regression_lab5_mai_andrew.py
+++ regression_lab5_mai_andrew.py
@@ -8,9 +8,9 @@ from sklearn.linear_model import Ridge
 
 training_percentage = 0.3
 
-data = loader.load("C:\\Users\\Luyen\\OneDrive\\Documents\\BCIT_WORK\\Machine Learning\\lab5\\data_lab5.csv")
+data = loader.load("C:\\Users\\Andrew\\PycharmProjects\\big_data_processing\\data_lab5.csv")
 data_expected = pd.read_csv(
-    "C:\\Users\\Luyen\\OneDrive\\Documents\\BCIT_WORK\\Machine Learning\\lab5\\data_lab5_expanded.csv")
+    "C:\\Users\\Andrew\\PycharmProjects\\big_data_processing\\data_lab5_expanded.csv")
 
 print(len(data), len(data_expected))
 
@@ -29,7 +29,7 @@ lin_reg = LinearRegression()
 
 lin_reg.fit(train_features, training_label)
 
-target_pred = lin_reg.predict(test_features)
+target_pred = lin_reg.predict(X=test_features)
 
 print(len(target_pred), len(test_label))
 
@@ -57,33 +57,36 @@ def poly_kfold_cv(x, y, p, k):
         ridge.fit(train_x, train_y)
         test_values = ridge.predict(validation_x)
         train_values = ridge.predict(train_x)
-
         train_error.append(np.mean(abs(train_values - train_y)))
         cv_error.append(np.mean(abs(test_values - validation_y)))
-    print(cv_error)
-    return np.mean(train_error), np.mean(cv_error)
+
+    train_error = np.mean(train_error)
+    cv_error = np.mean(cv_error)
+    return train_error, cv_error
 
 
-standard_data = data
-standard_set = data.drop('TARGET_D', axis=1, inplace=False)
-standard_test = data.loc[:, 'TARGET_D']
+standard_data = data_expected
+
 std_scalar = StandardScaler()
-standard_fit = std_scalar.fit_transform(standard_set)
 
-standard_train = standard_fit[:training_index]
-standard_test_set = standard_fit[training_index + 1:]
+standard_set = standard_data.drop('TARGET_D', axis=1, inplace=False)
+standard_test = standard_data.loc[:, 'TARGET_D']
+scalar_fit = std_scalar.fit_transform(standard_set)
+standard_train = scalar_fit[:training_index]
 standard_label = standard_test[:training_index]
 lam = [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-standard_train = pd.DataFrame(standard_train, columns=train_features.columns)
-print(len(standard_train), len(standard_test))
-train_error = []
-cv_error = []
+standard_fit = pd.DataFrame(standard_train, columns=standard_set.columns)
+
+print(len(standard_fit), len(standard_label))
+training_error = []
+cross_validation_error = []
 for i in lam:
-    te, cve = poly_kfold_cv(standard_train, standard_label, 10**i, 5)
-    train_error.append(te)
-    cv_error.append(cve)
-plt.plot(lam, cv_error, label="Cross Validation Error")
-plt.plot(lam, train_error, label="Training Error")
+    print("Processing lamda of 10^", i)
+    te, cve = poly_kfold_cv(standard_fit, standard_label, 10**i, 5)
+    training_error.append(te)
+    cross_validation_error.append(cve)
+plt.plot(lam, cross_validation_error, label="Cross Validation Error")
+plt.plot(lam, training_error, label="Training Error")
 plt.legend()
 plt.show()
