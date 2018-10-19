95dd6c5c6be9c0cdb1667b69dd21aa1ed0164b82
Arlios <luyen_mai@Live.com>
2018-10-18 21:26:16 -0700

    Regression edits

--- regression_lab5_mai_andrew.py
+++ regression_lab5_mai_andrew.py
@@ -46,6 +46,8 @@ def poly_kfold_cv(x, y, p, k):
     kframes = int(len(x) / k)
     train_error = []
     cv_error = []
+    ridge = Ridge(alpha=p)
+
     for i in range(k):
         index_start = i * kframes
         index_end = x.shape[0] - kframes * (k - i - 1)
@@ -53,28 +55,38 @@ def poly_kfold_cv(x, y, p, k):
         train_x = x[index_start:index_end]
         validation_x = x.drop(x.index[index_start:index_end])
         validation_y = y.drop(y.index[index_start:index_end])
-        ridge = Ridge(alpha=p)
-        ridge.fit(train_x, train_y)
+        ridge.fit(X=train_x, y=train_y)
         test_values = ridge.predict(validation_x)
         train_values = ridge.predict(train_x)
         train_error.append(np.mean(abs(train_values - train_y)))
         cv_error.append(np.mean(abs(test_values - validation_y)))
 
+    predict(x, y, p)
     train_error = np.mean(train_error)
     cv_error = np.mean(cv_error)
     return train_error, cv_error
 
 
+def predict(x, y, p):
+    ridge = Ridge(alpha=p)
+    ridge.fit(x, y)
+    prediction = ridge.predict(test_features)
+
+    prediction_error = np.mean(abs(prediction - test_label))
+    print("MAE at lamda: 10^", np.sqrt(p), prediction_error)
+
+
 standard_data = data_expected
 
-std_scalar = StandardScaler()
+scalar = StandardScaler(with_mean=0, with_std=1)
 
 standard_set = standard_data.drop('TARGET_D', axis=1, inplace=False)
 standard_test = standard_data.loc[:, 'TARGET_D']
-scalar_fit = std_scalar.fit_transform(standard_set)
+scalar_fit = scalar.fit_transform(standard_set)
 standard_train = scalar_fit[:training_index]
 standard_label = standard_test[:training_index]
 lam = [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
 
 standard_fit = pd.DataFrame(standard_train, columns=standard_set.columns)
 
@@ -86,6 +98,7 @@ for i in lam:
     te, cve = poly_kfold_cv(standard_fit, standard_label, 10**i, 5)
     training_error.append(te)
     cross_validation_error.append(cve)
+
 plt.plot(lam, cross_validation_error, label="Cross Validation Error")
 plt.plot(lam, training_error, label="Training Error")
 plt.legend()
